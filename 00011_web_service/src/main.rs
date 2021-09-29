86e3f2924dff9a04da2215cb7e5fcf3a48a2b8ad
MRW Neundorf <ninevillage@gmail.com>
2021-09-29 19:48:01 +0200

    feat(00011): implement web_service

--- 00011_web_service/src/main.rs
+++ 00011_web_service/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, World");
+use std::convert::Infallible;
+
+use mobc::{Connection, Pool};
+use mobc_postgres::{PgConnectionManager, tokio_postgres};
+use tokio_postgres::NoTls;
+use warp::Filter;
+
+mod data;
+mod db;
+mod error;
+mod handler;
+
+type DBCon = Connection<PgConnectionManager<NoTls>>;
+type DBPool = Pool<PgConnectionManager<NoTls>>;
+
+type Result<T> = std::result::Result<T, warp::Rejection>;
+
+fn with_db(db_pool: DBPool) -> impl Filter<Extract=(DBPool, ), Error=Infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
+}
+
+#[tokio::main]
+async fn main() {
+    let db_pool = db::create_pool().expect("database pool can be created");
+
+    db::init_db(&db_pool)
+        .await
+        .expect("database can be initialized");
+
+    let health_route = warp::path!("health")
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::health_handler);
+
+    let todo = warp::path("todo");
+    let todo_routes = todo
+        .and(warp::get())
+        .and(warp::query())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::list_todos_handler)
+        .or(todo
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::create_todo_handler))
+        .or(todo
+            .and(warp::put())
+            .and(warp::path::param())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::update_todo_handler))
+        .or(todo
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::delete_todo_handler));
+
+    let routes = health_route
+        .or(todo_routes)
+        .with(warp::cors().allow_any_origin())
+        .recover(error::handle_rejection);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
