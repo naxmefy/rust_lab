63eb1e1a14886fdfc3b3504ca2756e29954d21cc
MRW Neundorf <ninevillage@gmail.com>
2023-06-08 23:58:05 +0200

    feat(00015): init

---
+++ 00015_web_warp_and_vite_to_single_binary/README.md
@@ -0,0 +1,12 @@
+Based on https://fdeantoni.medium.com/single-rust-binary-with-vite-svelte-66944f9ac561
+
+## What I do not like here
+
+- the tutorial lacks in information 
+  - whole example can only be fullfilled with the [github example](https://github.com/zupzup/rust-frontend-example-yew)
+  - missing base infos to wasm (target installation etc.)
+
+
+## Missing
+
+- testing
---
+++ 00015_web_warp_and_vite_to_single_binary/web/src/Greeting.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+
+    const url = import.meta.env.PROD ? '/hello/svelte' : "http://localhost:3030/hello/svelte";
+
+    let greeting = "";
+
+    async function getHello() {
+        const opts = {
+            method: "get"
+        };
+        const response = await fetch(url, opts);
+        const result = await response.text();
+        console.log("Result:", result);
+
+        greeting = result;
+    }
+
+    onMount(() => getHello())
+</script>
+
+<h1>{greeting}</h1>
